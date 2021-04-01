5a5b28c365f02fcce83ddcb827c2c06f84987bdc
kranthi manupati <krmanupa@microsoft.com>
2021-04-01 22:17:35 +0530

    improvements to the script

--- CreateLinuxHWG.ps1
+++ CreateLinuxHWG.ps1
@@ -1,26 +1,71 @@
-<# 
- This runbook aids users in Creating Windows User Hybrid workers.
- This script must be executed on a Run As Enabled Automation Account only.   
- This would require the following modules to be present in the Automation account :  
- Az.Accounts, Az.Resources, Az.Automation, Az.OperationalInsights, Az.Compute 
-
- The script could even create (if needed) a Log Analaytics Workspace and also a VM to be registered as User Hybrid worker.
-#>
-
-
-
 <#
-    location : Location where you would want to create or get the LA Workspace from.
-    ResourceGroupName : ResourceGroup in which the automation account is present and where you want the resources created as part of this script lie.
-    AccountName : Automation Account name in which the Hybrid worker has to be registered.
-    CreateLA : True, creates a new LA Workspace with the given WorkspaceName in the given la location. False, Uses the given WorkspaceName for registering it as Hybrid worker.
-    lalocation: Location where the la workspace is present or has to be created. Default is the location given in the first parameter.
-    WorkspaceName : Name of the Log Analytics workspace, the script will create new one if not already present. Else, it enable "AzureAutomation" solution on the workspace.
-    CreateVM : True, creates a new VM (Ubuntu LTS) with the given VMName in the given VM location. False, Uses the given VMName for registering it as Hybrid worker.
-    vmName : Name of the VM
-    vmlocation : Location where the VM is present or has to be created. Default is the location given in the first parameter.
-    WorkerGroupName : Name of the Hybrid Worker Group. 
+.SYNOPSIS 
+    This sample automation runbook onboards Azure VMs for Azure Automation Hybrid Worker.
+    It can create a Azure VM or could use an exisiting VM to onboard as a Hybrid Worker.
+    Since onboarding a VM to Automation Hybrid Worker requires a Log Analytics workspace, this script also gives the feasibility to the users to provide an already exisiting Log Analytics workspace or the script could also create one for the users.
+    This Runbook needs to be run from the Automation account that you wish to connect the VM to.
+    
+    This script must be executed on a Run As Enabled Automation Account only.   
+    This would require the following modules to be present in the Automation account :  
+    Az.Accounts, Az.Resources, Az.Automation, Az.OperationalInsights, Az.Compute 
+ 
+.DESCRIPTION
+
+    This sample automation runbook onboards Azure VMs for Azure Automation Hybrid Worker.
+    It can create a Azure VM or could use an exisiting VM to onboard as a Hybrid Worker.
+    Since onboarding a VM to Automation Hybrid Worker requires a Log Analytics workspace, this script also gives the feasibility to the users to provide an already exisiting Log Analytics workspace or the script could also create one for the users.
+    This Runbook needs to be run from the Automation account that you wish to connect the VM to.
+    
+    This script must be executed on a Run As Enabled Automation Account only.   
+    This would require the following modules to be present in the Automation account :  
+    Az.Accounts, Az.Resources, Az.Automation, Az.OperationalInsights, Az.Compute 
+ 
+.PARAMETER Location
+    Required. Location of the automation account in which the script is executed.
+ 
+.PARAMETER ResourceGroupName
+    Required. The name of the resource group of the automation account.
+ 
+.PARAMETER AccountName
+    Required. The name of the autmation account in which the script is executed.
+ 
+.PARAMETER CreateLA 
+    Required. True, creates a new LA Workspace with the given WorkspaceName in the given LALocation. False, Uses the given WorkspaceName for Hybrid worker registration.
+ 
+.PARAMETER LAlocation
+    Optional. The location in which the LA Workspace to be used is present in or the location in which a new LA workspace has to be created in. 
+    If not provided the value will be used from the Location parameter.
+ 
+.PARAMETER WorkspaceName
+    Optional. The name of the LA workspace to be created or to be used for Hybrid worker registration.
+ 
+.PARAMETER CreateVM 
+    Required. True, creates a new VM with the given VMName in the given VMLocation. False, Uses the given VMName for Hybrid worker registration.
+
+.PARAMETER VMName
+    Optional. The name of the VM to be created or to be used to onboard as a Hybrid Worker.
+
+.PARAMETER VMImage
+    Optional. The name of the VM Image to be created.
+
+.PARAMETER VMlocation
+    Optional. The location in which the VM to be used is present in or the location in which a new VM has to be created in. 
+    If not provided the value will be used from the Location parameter.
+
+.PARAMETER RegisterHW
+    Required. True, Registers the provided VM as a Hybrid Worker. False, Doesn't register the VM as a Hybrdid Worker.
+
+.Example
+    .\CreateLinuxHWG  -location <location> -ResourceGroupName <ResourceGroupName> `
+     -AccountName <accountname> -CreateLA <$true/$false> -lalocation <lalocation> `
+     -WorkspaceName <WorkspaceName> -CreateVM <$true/$false> -vmName <vmName> -vmImage <VMImage> `
+     -RegisterHW <$true/$false> -vmlocation <vmlocation> -WorkerGroupName <HybridworkergroupName> 
+ 
+.NOTES
+    AUTHOR: Automation Team
+    LASTEDIT: March 31, 2021 
 #>
+
 
 Param(
     [Parameter(Mandatory = $true)]
@@ -28,23 +73,25 @@ Param(
     [Parameter(Mandatory = $true)]
     [string] $ResourceGroupName,
     [Parameter(Mandatory = $true)]
-    [string] $AccountName = "",
+    [string] $AccountName,
     [Parameter(Mandatory = $true)]
     [bool] $CreateLA,
     [Parameter(Mandatory = $false)]
     [String] $lalocation,
     [Parameter(Mandatory = $false)]
-    [string] $WorkspaceName = "LAWorkspaceForAutomationHW",
+    [string] $WorkspaceName,
     [Parameter(Mandatory = $true)]
     [bool] $CreateVM,
     [Parameter(Mandatory = $false)]
-    [String] $vmName = "VMForLHW",
+    [String] $vmName,
+    [Parameter(Mandatory = $false)]
+    [String] $vmImage,
     [Parameter(Mandatory = $true)]
     [bool] $RegisterHW,
     [Parameter(Mandatory = $false)]
     [String] $vmlocation,
     [Parameter(Mandatory = $false)]
-    [String] $WorkerGroupName = "Default-LinuxHWG"
+    [String] $WorkerGroupName
 )
  
 $ErrorActionPreference = "Stop"
@@ -55,8 +102,13 @@ $script:agentEndpoint = ""
 $script:aaPrimaryKey = ""
 $script:workspaceId = ""
 $script:workspacePrimaryKey = ""
-$script:vmlocation = $location
-$script:lalocation = $location
+
+if ([String]::IsNullOrEmpty($vmlocation)) {
+    $script:vmlocation = $location
+}
+if ([String]::IsNullOrEmpty($lalocation)) {
+    $script:lalocation = $location
+}
 
 function Login-AzAccount {
     $connectionName = "AzureRunAsConnection"
@@ -141,53 +193,117 @@ function New-VM {
         Write-Output "VM Already present. Skipping VM Creation."
         return
     }
-    #Create a VM
-    $vmNetworkName = "VMVnet" + $guid.SubString(0, 4)
-    $subnetName = "VMSubnet" + $guid.SubString(0, 4)
-    $newtworkSG = "VMNetworkSecurityGroup" + $guid.SubString(0, 4)
-    $ipAddressName = "VMPublicIpAddress" + $guid.SubString(0, 4)
-    $User = "VMUserLinux"
-    
-
-    $vmName = $vmName + $guid.SubString(0, 4)
-    $length = 12
-    Add-Type -AssemblyName System.Web 
-    $vmpassword = [System.Web.Security.Membership]::GeneratePassword($length, 2)
-    
-    $VMAccessingString = ConvertTo-SecureString $vmpassword -AsPlainText -Force
-    $VMCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $VMAccessingString
-
     try {
-        New-AzVm `
+        $vmNetworkName = "VMVnet" + $guid.SubString(0, 4)
+        $subnetName = "VMSubnet" + $guid.SubString(0, 4)
+        $newtworkSG = "VMNetworkSecurityGroup" + $guid.SubString(0, 4)
+    
+
+        # Define a credential object$length = 12
+        $User = "azureuser"
+        $length = 12
+        Add-Type -AssemblyName System.Web 
+        $vmpassword = [System.Web.Security.Membership]::GeneratePassword($length, 2)
+    
+        $VMAccessingString = ConvertTo-SecureString $vmpassword -AsPlainText -Force
+        $VMCredential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $User, $VMAccessingString
+
+
+        # subnet config
+        # Create a subnet configuration
+        $subnetConfig = New-AzVirtualNetworkSubnetConfig `
+            -Name $subnetName `
+            -AddressPrefix 192.168.1.0/24
+
+        # Create a virtual network
+        $vnet = New-AzVirtualNetwork `
             -ResourceGroupName $ResourceGroupName `
-            -Name $vmName `
-            -Location $vmlocation `
-            -VirtualNetworkName $vmNetworkName `
-            -SubnetName $subnetName `
-            -SecurityGroupName $newtworkSG `
-            -PublicIpAddressName $ipAddressName `
-            -Image "UbuntuLTS" `
-            -Credential $VMCredential | Out-Null
-        Start-Sleep -s 120
-        return
+            -Location $script:vmlocation `
+            -Name $vmNetworkName `
+            -AddressPrefix 192.168.0.0/16 `
+            -Subnet $subnetConfig
+
+        # Create a public IP address and specify a DNS name
+        $pip = New-AzPublicIpAddress `
+            -ResourceGroupName $ResourceGroupName `
+            -Location $script:vmlocation `
+            -AllocationMethod Static `
+            -IdleTimeoutInMinutes 4 `
+            -Name "mypublicdns$(Get-Random)"
+
+        # Create an inbound network security group rule for port 22
+        $nsgRuleSSH = New-AzNetworkSecurityRuleConfig `
+            -Name $newtworkSG `
+            -Protocol "Tcp" `
+            -Direction "Inbound" `
+            -Priority 1000 `
+            -SourceAddressPrefix * `
+            -SourcePortRange * `
+            -DestinationAddressPrefix * `
+            -DestinationPortRange 22 `
+            -Access "Allow"
+
+        # Create an inbound network security group rule for port 80
+        $nsgRuleWeb = New-AzNetworkSecurityRuleConfig `
+            -Name "myNetworkSecurityGroupRuleWWW"  `
+            -Protocol "Tcp" `
+            -Direction "Inbound" `
+            -Priority 1001 `
+            -SourceAddressPrefix * `
+            -SourcePortRange * `
+            -DestinationAddressPrefix * `
+            -DestinationPortRange 80 `
+            -Access "Allow"
+
+        # Create a virtual network card and associate with public IP address and NSG
+        $nic = New-AzNetworkInterface `
+            -Name "myNic" `
+            -ResourceGroupName $ResourceGroupName `
+            -Location $script:vmlocation `
+            -SubnetId $vnet.Subnets[0].Id `
+            -PublicIpAddressId $pip.Id `
+            -NetworkSecurityGroupId $nsg.Id
+
+        # Create a network security group
+        $nsg = New-AzNetworkSecurityGroup `
+            -ResourceGroupName $ResourceGroupName `
+            -Location $script:vmlocation `
+            -Name $newtworkSG `
+            -SecurityRules $nsgRuleSSH, $nsgRuleWeb
+
+        # Create a virtual machine configuration
+        $vmConfig = New-AzVMConfig `
+            -VMName $vmName ` | `
+            Set-AzVMOperatingSystem `
+            -Linux `
+            -ComputerName $vmName `
+            -Credential $VMCredential `
+            -DisablePasswordAuthentication | `
+            Add-AzVMNetworkInterface `
+            -Id $nic.Id
+
+
+
+        $newfile = New-Item -Path ~/.ssh/id_rsa.pub -Force
+        ssh-keygen -t rsa -f $(newfile.FullName) -P "TestPassPhrase" -Force
+
+        # Configure the SSH key
+        $sshPublicKey = cat $($newfile.FullName)
+        Add-AzVMSshPublicKey `
+            -VM $vmconfig `
+            -KeyData $sshPublicKey `
+            -Path "/home/azureuser/.ssh/authorized_keys"
+        #Create a VM
+        New-AzVM `
+            -ResourceGroupName $ResourceGroupName `
+            -Location $script:vmlocation -VM $vmConfig `
+            -Image $vmImage 
+    
     }
     catch {
-        $vmlocation = "West Europe"
-        Write-Output "Error creating VM retrying in $vmlocation..."
-        New-AzVm `
-            -ResourceGroupName $ResourceGroupName `
-            -Name $vmName `
-            -Location $vmlocation `
-            -VirtualNetworkName $vmNetworkName `
-            -SubnetName $subnetName `
-            -SecurityGroupName $newtworkSG `
-            -PublicIpAddressName $ipAddressName `
-            -Image "UbuntuLTS" `
-            -Credential $VMCredential | Out-Null
-        Start-Sleep -s 120
+        Write-Output "Error creating VM retrying in $vmlocation... $_"
     }
     
-    throw "Error Creating VM after 3 attempts"
 }
 
 function RegisterLinuxHW {
@@ -226,22 +342,20 @@ function RegisterLinuxHW {
 }
 
 
-Login-AzAccount
+# Login-AzAccount
 
-if ($CreateVM) {
+if ($CreateVM -eq $true) {
     #Create a VM
     try { 
-        if ($CreateVM -eq $true) {
-            Write-Output "Creating a new UbuntuLTS VM in $($vmlocation) with the provided details"
-            New-VM
-        }
+        Write-Output "Creating a new $($vmImage) VM in $($vmlocation) with the provided details"
+        New-VM
     }
     catch {
         Write-Error "HWG Creation :: Error creating VM : $_"
     }
 }
 
-if ($CreateLA) {
+if ($CreateLA -eq $true) {
     #Create an LA workspace
     try { 
         Write-Output "Creating a new LA Worksapce in $($lalocation) with the provided details"
@@ -252,7 +366,7 @@ if ($CreateLA) {
     }
 }
 
-if ($RegisterHW) {
+if ($RegisterHW -eq $true) {
     try {
         Write-Output "Fetching the automation account details for HW registration"
         Get-AutomationAccountDetails
